a8c67abe62b946a763d51e581cbabe48116883ec
vichik123 <eitan.vichik@gmail.com>
2024-01-17 20:19:56 +0200

    Add button binding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Shooter;
 
 /**
@@ -17,20 +19,27 @@ import frc.robot.subsystems.Shooter;
 public class RobotContainer {
 
     private final Shooter shooter = new Shooter();
+    private final CommandXboxController controller = new CommandXboxController(0);
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
-        SmartDashboard.putNumber("TopVelocity", 0);
-        SmartDashboard.putNumber("BottomVelocity", 0);
+        SmartDashboard.putNumber("Top Velocity", 0);
+        SmartDashboard.putNumber("Bottom Velocity", 0);
 
         // Configure the trigger bindings
         configureBindings();
     }
 
     private void configureBindings() {
-
+        controller
+                .rightTrigger(0.1)
+                .whileTrue(shooter.runShooter(
+                        () -> SmartDashboard.getNumber("Top Velocity", 0),
+                        () -> SmartDashboard.getNumber("Bottom Velocity", 0)
+                ))
+                .onFalse(shooter.runShooter(() -> 0));
     }
 
     /**
